402489aa2b53572fc8cd6a503ec5f6f92fff896c
Valerii Azarov <51995547+valerii-azarov@users.noreply.github.com>
2023-05-08 12:04:16 +0300

    Update firebase.ts

--- src/api/firebase.ts
+++ src/api/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "",
-    authDomain: "",
-    databaseURL: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: ""
+    apiKey: "AIzaSyD9vz7DWdOGb2STO8llcH9WaJrVtCerfHg",
+    authDomain: "kanban-board-22ff4.firebaseapp.com",
+    databaseURL: "https://kanban-board-22ff4-default-rtdb.firebaseio.com",
+    projectId: "kanban-board-22ff4",
+    storageBucket: "kanban-board-22ff4.appspot.com",
+    messagingSenderId: "643967209999",
+    appId: "1:643967209999:web:28087cb6936fc594da497a"
 };
   
 const app = initializeApp(firebaseConfig)
